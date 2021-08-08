c0f2b22e55d2f2cec2d44a4f19c4c8bc26445721
stephen <68674083+braincoolo@users.noreply.github.com>
2021-08-08 22:46:24 +0300

    Update music_stuff.js

--- music_stuff.js
+++ music_stuff.js
@@ -2,6 +2,7 @@ import Discord from 'discord.js';
 const {
     prefix,
     token,
-} = require('./config.json')
-const ytdl = require('ytdl-core')
+} = require('./config.json');
+const ytdl = require('ytdl-core');
 const client = new Discord.Client();
+client.login(token);
